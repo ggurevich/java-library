ed5992d447df4f0c6aad580d880f156c8c9c91d5
ggurevich <gurevichgabriel@gmail.com>
2016-10-14 13:16:58 -0400

    testing

--- src/_02_hello_world/test.java
+++ src/_02_hello_world/test.java
@@ -3,7 +3,11 @@ package _02_hello_world;
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		for(int i = 20; i >= 0; i -=2  ){
+			System.out.println(i);
+		}
+	       
 		
 		System.out.println("test");
 	}
